9d076029468d8b7266c6964a96c0a030d78dfff8
Gamaliel Barron <gbarronmm@gmail.com>
2020-01-03 10:33:16 -0800

    initial comment

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>This is going to be a bolierplate for react</p>, document.getElementById('app'));
+
+
+
